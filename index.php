f30300ee7c21e8ae542dec66f1f52796513d7837
James Ticer <jticer4@icloud.com>
2018-05-11 10:10:08 -0600

    added in fetch and set the output to be an unordered list that displays the id, title, and body for each of the posts

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8">
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+	<div id="output"></div>
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,17 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+	.then(res => res.json())
+	.then((data) => {
+	let output = '<h3> Posts </h3>';
+	data.forEach(function (user){
+		output += `
+			<ul>
+				<li>ID: ${user.id}</li>
+				<li>Title: ${user.title}</li>
+				<li>Body: ${user.body}</li>
+			</ul>
+			`;
+		document.getElementById('output').innerHTML = output
+	});
+	});
+
+
